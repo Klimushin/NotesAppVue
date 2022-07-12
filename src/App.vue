4eb62222a9099e9f14172af1df1ee41c0cf6c00f
Seroga <seroga2801@gmail.com>
2022-07-12 10:18:09 +0300

    add Notes app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+          <div class="container">
+            <h1> {{ title }} </h1>
+
+            <Message v-if="message" :message="message"/>
+
+            <NewNote
+            :note="note"
+            @addNote="addNote"/>
+
+             <!-- search -->
+            <Search
+                :value="search"
+                placeholder="Find your note"
+                @search="search = $event"
+            />
+
+            <Notes
+            :notes="notesFilter"
+            @remove="removeNote"/>
+
+          </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Message from '@/components/Message.vue'
+import NewNote from './components/NewNote.vue'
+import Search from "@/components/Search";
+import Notes from './components/Notes.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Message,
+    NewNote,
+    Search,
+    Notes
+},
+  data () {
+    return {
+      title: 'Notes App',
+      search: '',
+      message: null,
+      note: {
+          title: '',
+          descr: '',
+      },
+      notes: [
+          {
+              title: 'First note',
+              descr: 'Description for first note',
+              date: new Date(Date.now()).toLocaleString()
+          },
+          {
+              title: 'Second note',
+              descr: 'Description for second note',
+              date: new Date(Date.now()).toLocaleString()
+          },
+          {
+              title: 'Third note',
+              descr: 'Description for third note',
+              date: new Date(Date.now()).toLocaleString()
+          }
+      ]
+    }
+  },
+  computed: {
+    notesFilter () {
+      let array = this.notes,
+          search = this.search
+      if (!search) return array
+      search = search.trim().toLowerCase()
+      array = array.filter(function (item) {
+        if (item.title.toLowerCase().indexOf(search) !== -1) {
+          return item
+        }
+      })
+      return array
+    }
+  },
+  methods: {
+    addNote() {
+      let {title, descr} = this.note
+
+      if (title === '') {
+          this.message = 'Title can`t be blank!!!'
+          return false
+      }
+      this.notes.push({
+          title,
+          descr,
+          date: new Date(Date.now()).toLocaleString()
+      })
+      this.message = null
+      this.note.title = ''
+      this.note.descr = ''
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="message">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+ export default {
+    props: {
+        message: {
+            type: String,
+            required: true, 
+            // default: 'default msg'
+        }
+    }  
+}
+</script>
+
+<style lang="scss" scoped>
+.message {
+    text-align: center;
+    padding: 20px;
+}
+p {
+    color: rgb(233, 13, 134);
+}
+</style>
